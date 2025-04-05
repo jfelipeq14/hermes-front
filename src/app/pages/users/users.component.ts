99f020db637fe90d72c360218c203152ef2e43bc
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-04-05 11:18:51 -0500

    add users page

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,10 +1,165 @@
 /* eslint-disable @angular-eslint/component-class-suffix */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Table, TableModule } from 'primeng/table';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputTextModule } from 'primeng/inputtext';
+import { DialogModule } from 'primeng/dialog';
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { CalendarModule } from 'primeng/calendar';
+import { UserModel } from '../../models';
+import { UserService } from '../../services';
 
 @Component({
   selector: 'app-users',
-  imports: [],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css',
+  styleUrls: ['./users.component.css'],
+  providers: [MessageService, ConfirmationService, UserService],
+  standalone: true,
+  imports: [
+    CommonModule,
+    TableModule,
+    FormsModule,
+    ButtonModule,
+    ToastModule,
+    ToolbarModule,
+    InputTextModule,
+    DialogModule,
+    InputIconModule,
+    IconFieldModule,
+    ConfirmDialogModule,
+    CalendarModule,
+  ],
 })
-export class UsersPage {}
+export class UsersPage implements OnInit {
+  userDialog = false;
+  submitted = false;
+  user: UserModel = new UserModel();
+  users: UserModel[] = [];
+  loading = false;
+
+  constructor(
+    private userService: UserService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
+
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
+
+  openNew() {
+    this.user = new UserModel();
+    this.submitted = false;
+    this.userDialog = true;
+  }
+
+  editUser(user: UserModel) {
+    this.user = { ...user };
+    this.userDialog = true;
+  }
+
+  deleteUser(user: UserModel) {
+    this.confirmationService.confirm({
+      message:
+        '¿Está seguro de que desea cambiar el estado de ' + user.name + '?',
+      header: 'Confirmar',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        user.status = !user.status;
+        this.userService.update(user).subscribe({
+          next: () => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Éxito',
+              detail: 'Estado del servicio actualizado',
+            });
+            this.getAllUsers();
+          },
+          error: () => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: 'No se pudo actualizar el estado del servicio',
+            });
+          },
+        });
+      },
+    });
+  }
+
+  getAllUsers() {
+    this.loading = true;
+    this.userService.getAll().subscribe({
+      next: (users) => {
+        this.users = users;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'No se pudieron cargar los servicios',
+        });
+      },
+    });
+  }
+
+  saveUser() {
+    this.submitted = true;
+
+    if (this.user.name.trim()) {
+      if (this.user.id) {
+        this.userService.update(this.user).subscribe({
+          next: () => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Éxito',
+              detail: 'Servicio actualizado',
+            });
+            this.getAllUsers();
+          },
+          error: () => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: 'No se pudo actualizar el servicio',
+            });
+          },
+        });
+      } else {
+        this.userService.create(this.user).subscribe({
+          next: () => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Éxito',
+              detail: 'Servicio creado',
+            });
+            this.getAllUsers();
+          },
+          error: () => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: 'No se pudo crear el servicio',
+            });
+          },
+        });
+      }
+
+      this.userDialog = false;
+      this.user = new UserModel();
+    }
+  }
+}
