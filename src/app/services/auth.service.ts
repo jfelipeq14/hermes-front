fadf9d4f6b3344d9e6c168c10f9cd9c365ffdf0a
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-04-07 10:26:20 -0500

    auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,37 +1,36 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthService {}
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ActivityModel } from '../models';
 import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
-  private url = environment.SERVER_URL + 'activities/';
+  private url = environment.SERVER_URL + 'auth/';
 
-  getAll(): Observable<ActivityModel[]> {
-    return this.http.get<ActivityModel[]>(this.url);
+  LoginModel = {
+    email: '',
+    password: '',
+  };
+
+  RegisterModel = {
+    idRole: 0,
+    typeDocument: '',
+    document: '',
+    name: '',
+    surName: '',
+    idMunicipality: 0,
+    phone: '',
+    email: '',
+    password: '',
+  };
+
+  login(): Observable<any> {
+    return this.http.post<any>(this.url + 'log-in', this.LoginModel);
   }
 
-  getById(id: number): Observable<ActivityModel> {
-    return this.http.get<ActivityModel>(this.url + id);
-  }
-
-  create(activity: ActivityModel): Observable<ActivityModel> {
-    return this.http.post<ActivityModel>(this.url, activity);
-  }
-
-  update(activity: ActivityModel): Observable<ActivityModel> {
-    return this.http.put<ActivityModel>(this.url + activity.id, activity);
-  }
-
-  changeStatus(id: number): Observable<ActivityModel> {
-    return this.http.patch<ActivityModel>(this.url + id, {});
+  register(): Observable<any> {
+    return this.http.post<any>(this.url + 'sign-up', this.RegisterModel);
   }
 }
