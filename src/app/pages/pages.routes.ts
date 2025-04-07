4c0944bd243bc58007996a5d35a98c7001deeedb
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-04-07 07:31:11 -0500

    create calendar

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -1,8 +1,24 @@
 import { Routes } from '@angular/router';
+import { DashboardPage } from './dashboard/dashboard.component';
+import { RolesPage } from './roles/roles.component';
+import { UsersPage } from './users/users.component';
+import { CategoriesPage } from './categories/categories.component';
+import { ServicesPage } from './services/services.component';
+import { ActivitiesPage } from './activities/activities.component';
+import { PackagesPage } from './packages/packages.component';
+import { ProgrammingPage } from './programming/programming.component';
 
 export default [
+  { path: 'dashboard', component: DashboardPage },
+  { path: 'roles', component: RolesPage },
+  { path: 'users', component: UsersPage },
+  { path: 'categories', component: CategoriesPage },
+  { path: 'services', component: ServicesPage },
+  { path: 'activities', component: ActivitiesPage },
+  { path: 'packages', component: PackagesPage },
+  { path: 'programming', component: ProgrammingPage },
   {
     path: '**',
-    redirectTo: '/',
+    redirectTo: 'home',
   },
 ] as Routes;
