f86032a0dc003ee072eaf61150cb6c4150070f71
Bryan0813 <brayansanchez0813@gmail.com>
2025-04-14 19:47:36 -0500

    Add client-form

---
+++ src/app/shared/components/form-travelers/form-travelers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-travelers',
+  imports: [],
+  templateUrl: './form-travelers.component.html',
+  styleUrl: './form-travelers.component.css'
+})
+export class FormTravelersComponent {
+
+}
