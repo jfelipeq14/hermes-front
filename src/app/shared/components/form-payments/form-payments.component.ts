a5dc54459c794a721c37b425dd710347cdccdec2
Bryan0813 <brayansanchez0813@gmail.com>
2025-04-15 20:00:19 -0500

    Add form-payments

---
+++ src/app/shared/components/form-payments/form-payments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-payments',
+  imports: [],
+  templateUrl: './form-payments.component.html',
+  styleUrl: './form-payments.component.css'
+})
+export class FormPaymentsComponent {
+
+}
