c3a2abd0518108b3737a782bcb47c85f12470524
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-04-17 14:40:20 -0500

    refactor calendar component: update styles, enhance event display, and add context menu options

--- src/app/shared/components/calendar/calendar.component.ts
+++ src/app/shared/components/calendar/calendar.component.ts
@@ -1,26 +1,62 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, signal, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { CalendarOptions } from '@fullcalendar/core';
 import interactionPlugin from '@fullcalendar/interaction';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import { FullCalendarModule } from '@fullcalendar/angular';
-import { CommonModule } from '@angular/common';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { TabsModule } from 'primeng/tabs';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+
 import { DateModel } from '../../../models';
 import { MessageService } from 'primeng/api';
 import { ProgrammingService } from '../../../services';
+
+interface MenuItem {
+  label: string;
+  icon: string;
+  command?: () => void;
+}
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css'],
-  imports: [CommonModule, FullCalendarModule],
+  imports: [CommonModule, FullCalendarModule, MenuModule, ButtonModule],
   providers: [ProgrammingService, MessageService],
 })
 export class CalendarComponent implements OnInit {
   constructor(private programmingService: ProgrammingService) {}
 
+  selectedEvent: any;
   dates: DateModel[] = [];
+  overlayMenuItems: MenuItem[] = [
+    {
+      label: 'Editar programación',
+      icon: 'pi pi-fw pi-pencil',
+    },
+    {
+      label: 'Cambiar estado',
+      icon: 'pi pi-fw pi-sync',
+    },
+    {
+      label: 'Crear reserva',
+      icon: 'pi pi-fw pi-calendar-plus',
+    },
+    {
+      label: 'Ver clientes',
+      icon: 'pi pi-fw pi-user',
+    },
+  ];
 
   ngOnInit(): void {
     this.getAllDates();
@@ -36,7 +72,7 @@ export class CalendarComponent implements OnInit {
             title: `Paquete ${date.idPackage}`,
             start: date.start,
             end: date.end,
-            allDay: true,
+            allDay: false,
           })),
         }));
       },
@@ -64,7 +100,7 @@ export class CalendarComponent implements OnInit {
       title: `Paquete ${date.idPackage}`,
       start: date.start,
       end: date.end,
-      allDay: true,
+      allDay: false,
     })),
   });
 }
