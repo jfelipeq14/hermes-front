634bfdcac2d6620845405cda65da3a92c0b2eff0
Bryan0813 <brayansanchez0813@gmail.com>
2025-04-20 00:43:57 -0500

    feat(reservations): refactor reservation handling and integrate traveler creation

--- src/app/services/reservations.service.ts
+++ src/app/services/reservations.service.ts
@@ -4,38 +4,50 @@ import { Observable } from 'rxjs';
 
 import { ReservationModel } from '../models/reservation';
 import { environment } from '../../environments/environment';
+import { ReservationTravelerModel } from '../models';
 
 @Injectable()
 export class ReservationsService {
   constructor(private http: HttpClient) {}
 
-  private url = environment.SERVER_URL + 'reservations/';
+  private urlReservation = environment.SERVER_URL + 'reservations/';
+  private urlTraveler = environment.SERVER_URL + 'travelers/';
 
   // Obtener todas las reservaciones
   getAll(): Observable<ReservationModel[]> {
-    return this.http.get<ReservationModel[]>(this.url);
+    return this.http.get<ReservationModel[]>(this.urlReservation);
   }
 
   // Obtener una reservación por ID
   getById(id: number): Observable<ReservationModel> {
-    return this.http.get<ReservationModel>(`${this.url}${id}`);
+    return this.http.get<ReservationModel>(`${this.urlReservation}${id}`);
   }
 
   // Crear una nueva reservación
   create(reservation: ReservationModel): Observable<ReservationModel> {
-    return this.http.post<ReservationModel>(this.url, reservation);
+    console.log('create', reservation);
+    return this.http.post<ReservationModel>(this.urlReservation, reservation);
+  }
+
+  createTraveler(
+    travelers: ReservationTravelerModel[]
+  ): Observable<ReservationTravelerModel[]> {
+    return this.http.post<ReservationTravelerModel[]>(
+      this.urlTraveler,
+      travelers
+    );
   }
 
   // Actualizar una reservación existente
   update(reservation: ReservationModel): Observable<ReservationModel> {
     return this.http.patch<ReservationModel>(
-      `${this.url}${reservation.id}`,
+      `${this.urlReservation}${reservation.id}`,
       reservation
     );
   }
 
   // Cambiar el estado de una reservación
   changeStatus(id: number): Observable<ReservationModel> {
-    return this.http.patch<ReservationModel>(`${this.url}${id}`, {});
+    return this.http.patch<ReservationModel>(`${this.urlReservation}${id}`, {});
   }
 }
