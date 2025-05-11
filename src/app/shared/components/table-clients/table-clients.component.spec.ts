6dcbe55de874adef0b46bd0b0eb637f9573acc19
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-05-11 17:11:19 -0500

    feat: add table component for displaying clients and integrate reservations service

---
+++ src/app/shared/components/table-clients/table-clients.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TableClientsComponent } from './table-clients.component';
+
+describe('TableClientsComponent', () => {
+  let component: TableClientsComponent;
+  let fixture: ComponentFixture<TableClientsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TableClientsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TableClientsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
