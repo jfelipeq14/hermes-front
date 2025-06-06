8ece9ebd1ab86bdf91350615ac905364756d4f8d
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-06-06 18:10:21 -0500

    refactor: enhance hasRoleGuard to handle user redirection more effectively

--- src/app/guards/has-role.guard.ts
+++ src/app/guards/has-role.guard.ts
@@ -3,19 +3,24 @@ import { CanActivateFn } from '@angular/router';
 import { of, switchMap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
-export const hasRoleGuard = (): CanActivateFn => {
+export const hasRoleGuard = (allowedRoleIds: number[]): CanActivateFn => {
     return () => {
         const authService = inject(AuthService);
 
         return authService.currentUser$.pipe(
             switchMap((user) => {
-                if (user) {
-                    authService.redirectBasedOnRole();
+                if (!user) {
+                    window.location.href = '/landing';
                     return of(false);
                 }
 
-                window.location.href = '/landing';
-                return of(false);
+                authService.redirectBasedOnRole();
+
+                const hasRole = allowedRoleIds.includes(user.idRole);
+                if (!hasRole) {
+                    window.location.href = '/landing';
+                }
+                return of(hasRole);
             })
         );
     };
