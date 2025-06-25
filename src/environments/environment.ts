98838faa9fbdb55d3d03a383d4eb0fdf1244b1df
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2025-06-25 14:16:55 -0500

    feat: Update environment URLs for production deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    SERVER_URL: 'http://localhost:3000/',
-    CLIENT_URL: 'http://localhost:4200/'
+    SERVER_URL: 'https://hermes-backend-production.up.railway.app/',
+    CLIENT_URL: 'https://hermes-front-eight.vercel.app/'
 };
